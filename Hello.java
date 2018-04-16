a5b46d7a7abc55dc3f796f66803a76c944325598
pulykaman <35999258+pulykaman@users.noreply.github.com>
2018-04-16 12:09:16 +0200

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("init commit test");
+		System.out.println("init commit test1");
+		System.out.println("init commit test2");
+		System.out.println("init commit test3");
 	}
 }
